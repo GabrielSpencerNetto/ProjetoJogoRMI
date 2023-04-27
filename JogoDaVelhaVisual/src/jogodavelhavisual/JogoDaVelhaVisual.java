59f22280591d43ec83e15d68a105acb92345509c
GabrielSpencerNetto <gabrielspencernetto@gmail.com>
2023-04-27 12:50:32 -0300

    Add files via upload

---
+++ JogoDaVelhaVisual/src/jogodavelhavisual/JogoDaVelhaVisual.java
@@ -0,0 +1,14 @@
+package jogodavelhavisual;
+
+/**
+ *
+ * @author Gabriel Spencer
+ */
+public class JogoDaVelhaVisual {
+
+    public static void main(String[] args) {
+        VelhaFrame vf = new VelhaFrame();
+        vf.setVisible(true);
+    }
+    
+}
